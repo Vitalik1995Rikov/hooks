b8b65a4a66bdd1afa541fdbd8f6c9d23b9a9f9b5
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-12 17:41:13 +0300

    useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -58,18 +58,19 @@ const Slider = (props) => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-                <div className="buttons mt-3">
+                <div className="text-center mt-5">Active slide {slide} <br/> </div>
+                {/* <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => this.changeSlide(-1)}>-1</button>
+                        onClick={() => this.changeSlide(-1)}>-1
+                        </button>
                     <button 
                         className="btn btn-primary me-2"
                         onClick={() => this.changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
                         onClick={this.toggleAutoplay}>toggle autoplay</button>
-                </div>
+                </div> */}
             </div>
         </Container>
     )
