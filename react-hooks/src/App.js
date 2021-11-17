55e83cfc1371700adb4fd17c9949a17f93a7519d
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-17 20:15:10 +0300

    useMemo

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -51,15 +51,9 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                {/* {
-                    getSomeImages().map((url, i) => {
-                        return (
-                            <img key={i} className="d-block w-100" src={url} alt="slide" />
-                        )
-                    })
-                } */}
                 <Slide getSomeImages={getSomeImages}/>
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
+                <div className="text-center mt-5">Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
