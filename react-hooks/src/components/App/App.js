bc1941efac21cd5f34c55e0309e00504633a7433
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-25 18:58:33 +0300

    added useState for top and left

--- react-hooks/src/components/App/App.js
+++ react-hooks/src/components/App/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Rectangle } from '../Rectangle/Rectangle';
 import styles from './App.module.css';
 
 const App = (props) => {
 
+    const [top, setTop] = useState(10);
+    const [left, setLeft] = useState(10);
+
     function handleMouseMove(e) {
-        console.log(e)
+        const { movementX, movementY } = e;
+        
+        console.log()
+        setLeft(left + movementX);
+        setTop(top + movementY);
     }
 
     return (
         <div className={styles.wrapper}
             onMouseMove={handleMouseMove}>
             <Rectangle
-            top={80}
-            left={80}
+            top={top}
+            left={left}
             />
         </div>
     )
