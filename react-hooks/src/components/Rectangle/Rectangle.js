16ac627ebdfbafbe0b3efcb5cdf71f4d280e516f
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-25 18:30:57 +0300

    install proptypes

--- react-hooks/src/components/Rectangle/Rectangle.js
+++ react-hooks/src/components/Rectangle/Rectangle.js
@@ -1,11 +1,15 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styles from './Rectangle.module.css';
 
 const Rectangle = (props) => {
+    const { top, left } = props;
     const style = {
         width: 80,
         height: 80,
-        backgroundColor: 'red'
+        backgroundColor: 'red',
+        top,
+        left
     }
 
     return (
@@ -13,6 +17,11 @@ const Rectangle = (props) => {
             Rectangle
         </div>
     )
+}
+
+Rectangle.propTypes = {
+    top: PropTypes.number,
+    left: PropTypes.number
 }
 
 export {Rectangle};
